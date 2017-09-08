889466586aa58fb27e485d088cefdd6bee98aa52
AlexWalker28 <alexwolk28@gmail.com>
2017-09-08 22:12:07 +0600

    Initial commit

---
+++ app/src/main/java/kg/kloop/android/zvonilka/zvonilka/MainActivity.java
@@ -0,0 +1,25 @@
+package kg.kloop.android.zvonilka.zvonilka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+
+    }
+}
