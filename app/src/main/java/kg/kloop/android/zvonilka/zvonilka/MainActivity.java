adf72ec1d1ee3a657ac1d49af5ce38ec7e1b094d
AlexWalker28 <alexwolk28@gmail.com>
2017-09-11 22:30:06 +0600

    Add floation action button and some dummy data

--- app/src/main/java/kg/kloop/android/zvonilka/zvonilka/MainActivity.java
+++ app/src/main/java/kg/kloop/android/zvonilka/zvonilka/MainActivity.java
@@ -2,14 +2,24 @@ package kg.kloop.android.zvonilka.zvonilka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+    ArrayList<Campaign> campaignArrayList;
+    Campaign campaign = new Campaign("example title", "test description");
+    Campaign campaign1 = new Campaign("this title", "another description");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        campaignArrayList = new ArrayList<>();
+        campaignArrayList.add(campaign);
+        campaignArrayList.add(campaign1);
 
+        adapter = new RecyclerViewAdapter(campaignArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
+
+
 }
