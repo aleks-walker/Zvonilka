357ae5cc6e533dcd43988621ac2b3fd877d5e248
AlexWalker28 <alexwolk28@gmail.com>
2017-09-12 21:54:11 +0600

    Make layout for campaign activity

--- app/src/main/java/kg/kloop/android/zvonilka/CampaignActivity.java
+++ app/src/main/java/kg/kloop/android/zvonilka/CampaignActivity.java
@@ -2,6 +2,7 @@ package kg.kloop.android.zvonilka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CampaignActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class CampaignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
