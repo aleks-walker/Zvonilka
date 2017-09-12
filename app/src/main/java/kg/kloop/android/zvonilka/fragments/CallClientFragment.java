7f3d207f1a0ef931a275f8639084e4fa21d399db
AlexWalker28 <alexwolk28@gmail.com>
2017-09-12 22:36:53 +0600

    Make layout for call client fragment

--- app/src/main/java/kg/kloop/android/zvonilka/fragments/CallClientFragment.java
+++ app/src/main/java/kg/kloop/android/zvonilka/fragments/CallClientFragment.java
@@ -2,7 +2,9 @@ package kg.kloop.android.zvonilka.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,8 @@ import kg.kloop.android.zvonilka.R;
  */
 public class CallClientFragment extends Fragment {
 
+    RecyclerView clientsToCallRecyclerView;
+    FloatingActionButton addClientFloatingActionButton;
 
     public CallClientFragment() {
         // Required empty public constructor
@@ -24,8 +28,19 @@ public class CallClientFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_call_client, container, false);
+        View view = inflater.inflate(R.layout.fragment_call_client, container, false);
+
+        clientsToCallRecyclerView = view.findViewById(R.id.clients_to_call_recycler_view);
+        addClientFloatingActionButton = view.findViewById(R.id.add_client_floating_action_button);
+
+        addClientFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/kg/kloop/android/zvonilka/objects/Client.java
@@ -0,0 +1,9 @@
+package kg.kloop.android.zvonilka.objects;
+
+/**
+ * Created by alexwalker on 12.09.17.
+ */
+
+public class Client {
+
+}
